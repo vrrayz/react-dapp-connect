a52d5c54251a055b6dc7585d7e5e38d49c60e7c3
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-21 05:10:53 +0100

    mobile nav css done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 import { ethers } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
+
+import { walletConnectRpc } from "./rpc";
+
 function App() {
   const [isConnected, setIsConnected] = useState(false);
   const [userAddress, setUserAddress] = useState('')
   const [provider, setProvider] = useState(new ethers.providers.Web3Provider(window.ethereum))
 
   const connectDapp = async() => {
-    const wcProvider = new WalletConnectProvider({
-      rpc: {
-        97: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-        // 56: "https://bsc-dataseed.binance.org/",
-      },
-    })
+    const wcProvider = new WalletConnectProvider(walletConnectRpc)
     await wcProvider.enable()
     // await provider.send("eth_requestAccounts", []);
     setProvider(new ethers.providers.Web3Provider(wcProvider));
@@ -22,7 +20,10 @@ function App() {
   }
   return (
     <>
-      <h1>React - Dapp Connect</h1>
+      <nav className="custom-nav">
+        <a href="/h" className="nav-brand">Dapp-Connect</a>
+        <button className="btn nav-btn">Connect Wallet</button>
+      </nav>
 
       <div>
         {isConnected ? <b>{userAddress}</b>:<button onClick={connectDapp}>Connect Dapp</button>}
