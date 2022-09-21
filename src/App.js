fa24d9e0951b32c639ac70844342cc19f60c0fc2
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-21 04:32:24 +0100

    dapp connect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [userAddress, setUserAddress] = useState('')
+  const [provider, setProvider] = useState(new ethers.providers.Web3Provider(window.ethereum))
+
+  const connectDapp = async() => {
+    const wcProvider = new WalletConnectProvider({
+      rpc: {
+        97: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+        // 56: "https://bsc-dataseed.binance.org/",
+      },
+    })
+    await wcProvider.enable()
+    // await provider.send("eth_requestAccounts", []);
+    setProvider(new ethers.providers.Web3Provider(wcProvider));
+    const signer = provider.getSigner()
+    signer.getAddress().then(res => setUserAddress(res))
+    setIsConnected(true)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React - Dapp Connect</h1>
+
+      <div>
+        {isConnected ? <b>{userAddress}</b>:<button onClick={connectDapp}>Connect Dapp</button>}
+      </div>
+    </>
   );
 }
 
