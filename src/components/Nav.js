334dc5ca2f9400f567882bb332c8f3f04f2d6ea2
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-21 12:01:46 +0100

    Nav props refactor

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Nav = (props) => {
+const Nav = ({setIsModalToggled,isModalToggled,isConnected,userAddress}) => {
   return (
     <nav className="custom-nav">
         <a href="/h" className="nav-brand">Dapp-Connect</a>
-        {props.children}
+        <button
+          className="btn nav-btn"
+          onClick={() => setIsModalToggled(!isModalToggled)}
+        >
+          {isConnected ? `${userAddress.substring(0, 5)}...` : "Connect Dapp"}
+        </button>
       </nav>
   )
 }
